2a5c4f2e1d4738613467d6089afdcd21f3359c66
Rabiul Islam <rabiul.robi2023@gmail.com>
2025-10-02 11:56:01 +0600

    Dynamic metadata added in dynamic blog page

--- src/app/(public)/blogs/[blogId]/page.tsx
+++ src/app/(public)/blogs/[blogId]/page.tsx
@@ -1,7 +1,23 @@
 import BlogDetailsCard from "@/components/modules/Blogs/BlogDetailsCard";
 import { IBlog } from "@/types";
 import { Metadata } from "next";
+
 import React from "react";
+
+export const generateMetadata = async ({
+  params,
+}: {
+  params: Promise<{ blogId: string }>;
+}): Promise<Metadata> => {
+  const { blogId } = await params;
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post/${blogId}`);
+  const blog: IBlog = await res.json();
+
+  return {
+    title: blog?.title,
+    description: blog?.content,
+  };
+};
 
 const BlogDeatailsPage = async ({
   params,
